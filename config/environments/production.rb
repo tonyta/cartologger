c565fd71b8ccd2bff2770b1c6f816eebe6fb8fcf
Tony Ta <tonyta.tt@gmail.com>
2016-05-28 23:11:05 -0700

    configure domain for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://#{ ENV["DOMAIN_NAME"] }/cable"
+  config.action_cable.allowed_request_origins = [ "http://#{ ENV["DOMAIN_NAME"] }", /http:\/\/#{ ENV["DOMAIN_NAME"] }/ ]
 
   # Don't mount Action Cable in the main server process.
   # config.action_cable.mount_path = nil
